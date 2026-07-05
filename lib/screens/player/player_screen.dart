0c8e1ee6c6eab47086d281da251a05ff194c7da4
naveen <naveenp6655@gmail.com>
2026-07-05 20:28:09 +0530

    Added music player UI with audio playback and CD rotation

--- lib/screens/player/player_screen.dart
+++ lib/screens/player/player_screen.dart
@@ -8,21 +8,124 @@ class PlayerScreen extends StatefulWidget {
   State<PlayerScreen> createState() => _PlayerScreenState();
 }
 
-class _PlayerScreenState extends State<PlayerScreen> {
+class _PlayerScreenState extends State<PlayerScreen>
+    with SingleTickerProviderStateMixin {
   final AudioPlayer player = AudioPlayer();
+
+  bool isPlaying = false;
+
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 8),
+    );
+
+    _controller.stop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF0D1B2A),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await player.play(
-              AssetSource('sounds/click.mp3'),
-            );
-          },
-          child: const Text("Play Song"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RotationTransition(
+              turns: _controller,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'assets/image/deva.png',
+                  width: 250,
+                  height: 250,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            const Text(
+              "Deva",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            const Text(
+              "Tamil Song",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 18,
+              ),
+            ),
+
+            const SizedBox(height: 40),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.skip_previous,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+
+                const SizedBox(width: 20),
+
+                CircleAvatar(
+                  radius: 35,
+                  backgroundColor: Colors.white,
+                  child: IconButton(
+                    onPressed: () async {
+                      if (isPlaying) {
+                        await player.pause();
+                        _controller.stop();
+                      } else {
+                        await player.play(
+                          AssetSource('sounds/click.mp3'),
+                        );
+                        _controller.repeat();
+                      }
+
+                      setState(() {
+                        isPlaying = !isPlaying;
+                      });
+                    },
+                    icon: Icon(
+                      isPlaying ? Icons.pause : Icons.play_arrow,
+                      color: Colors.black,
+                      size: 40,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(width: 20),
+
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.skip_next,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -30,6 +133,7 @@ class _PlayerScreenState extends State<PlayerScreen> {
 
   @override
   void dispose() {
+    _controller.dispose();
     player.dispose();
     super.dispose();
   }
